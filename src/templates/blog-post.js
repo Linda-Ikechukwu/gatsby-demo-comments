b128516298a2ea49866ba7f54d3eb9a6622f5420
Linda-Ikechukwu <lindaedwin96@gmail.com>
2020-10-03 13:38:16 +0100

    add commentsbox

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,27 +1,38 @@
-import React from "react"
+import React, {useEffect} from "react"
 import { Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+//FOR GITTALK
 /* import Gitalk from 'gatsby-plugin-gitalk';
 import '@suziwen/gitalk/dist/gitalk.css' */
 
-import Comments from 'remark-ninja-react'
+
+//FOR REMARK
+/* import Comments from 'remark-ninja-react' */
+
+import commentBox from 'commentbox.io';
 
 const BlogPostTemplate = ({ data, pageContext, location }) => {
   const post = data.markdownRemark
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const { previous, next } = pageContext
 
+  //CONFIG FOR GITTALK
   /* let gitalkConfig = {
     id: post.id,
     title: post.frontmatter.title,
   } */
 
-  const siteID = '12dcbf8c-34a0-480f-8ec3-718de293b101';
-  const threadSlug = post.frontmatter.title;
+  // CONFIG FOR REMARK
+  /* const siteID = '12dcbf8c-34a0-480f-8ec3-718de293b101';
+  const threadSlug = post.frontmatter.title; */
+
+  useEffect(() =>{
+    commentBox('5632596591509504-proj')
+  })
 
   return (
     <Layout location={location} title={siteTitle}>
@@ -48,9 +59,13 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
         </footer>
       </article>
 
+      {/* //FOR GITTALK */}
       {/* <Gitalk options={gitalkConfig}/> */}
 
-      <Comments siteId={siteID} threadSlug={threadSlug} />
+      {/* //FOR REMARK */}
+      {/* <Comments siteId={siteID} threadSlug={threadSlug} /> */}
+
+      <div className="commentbox" />
 
       <nav className="blog-post-nav">
         <ul
